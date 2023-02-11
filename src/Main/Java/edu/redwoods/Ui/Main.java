d545ba5b842ac5eda5f2023f1a485754ee76deb4
Lokki699 <uriahmc96@gmail.com>
2023-02-10 20:17:36 -0800

    Yes

---
+++ src/Main/Java/edu/redwoods/Ui/Main.java
@@ -0,0 +1,9 @@
+package edu.redwoods.Ui;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args)  {
+        Application.launch(Ui.class);
+    }
+}
